863f95cec2c4e1054ddf86d6e8122140aae92f23
fayzab <144379923+fayzab@users.noreply.github.com>
2024-04-26 22:33:57 -0400

    Delete NotificationManager.java

--- NotificationManager.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class NotificationManager {
-    public static void notifyUser(String message) {
-        JOptionPane.showMessageDialog(null, message, "New Message Notification", JOptionPane.INFORMATION_MESSAGE);
-    }
-}
